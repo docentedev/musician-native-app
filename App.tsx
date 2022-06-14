7cc62e806dabd54acec3d0a4f885e7e93c0fef28
docente.dev <docente.dev@gmail.com>
2022-06-14 08:46:44 -0400

    feat: v1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const getMusicians = async () => {
+  const response = await fetch('https://musician-api-production.up.railway.app/api/v1/musicians');
+  const data = await response.json();
+  return data;
+}
+
 export default function App() {
+  const [musicians, setMusicians] = useState([]);
+  useEffect(() => {
+    getMusicians().then(data => {
+      setMusicians(data.rows);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Musicians</Text>
+      {musicians.map(musician => (
+        <Text key={musician.id}>{musician.first_name}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
